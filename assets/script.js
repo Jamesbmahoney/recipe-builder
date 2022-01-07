cadcefd42298439890780907f74ab261e79a62b6
James Barry <jbm03003@gmail.com>
2022-01-06 20:47:23 -0500

    uncommented getRecipe function

--- assets/script.js
+++ assets/script.js
@@ -3,28 +3,28 @@
 
 
 
-//  function getRecipe() {
-    // var apiUrl = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=62e75e598b60470591e3fd45554b92ca&ingredients=apples,+flour,+sugar&number=2";
-    //   fetch(apiUrl)
-        // .then(function (response){
-            // if (response.ok) {
-                // response.json().then(function(data) {
-                    // console.log(data);
+ function getRecipe() {
+    var apiUrl = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=62e75e598b60470591e3fd45554b92ca&ingredients=apples,+flour,+sugar&number=2";
+      fetch(apiUrl)
+        .then(function (response){
+            if (response.ok) {
+                response.json().then(function(data) {
+                    console.log(data);
 
-                    // displayRecipe(data);
+                    displayRecipe(data);
                     
-                // })
-            // }
-        // })
-// }
+                })
+            }
+        })
+}
 
-// function displayRecipe(recipe) {
-    // for (var i = 0; i < recipe.length; i++) {
-        // var recipeImage = recipe[i].image;
-        // var recipeName = recipe[i].title;
-        // console.log(recipeImage, recipeName);
-    // }
-// }
+function displayRecipe(recipe) {
+    for (var i = 0; i < recipe.length; i++) {
+        var recipeImage = recipe[i].image;
+        var recipeName = recipe[i].title;
+        console.log(recipeImage, recipeName);
+    }
+}
 
 function getDrink() {
     var apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php?apiKey=1";
@@ -46,7 +46,7 @@ function getDrink() {
 }
 
 
-// getRecipe();
+ getRecipe();
 getDrink();
 
 

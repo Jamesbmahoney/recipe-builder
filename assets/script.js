d3dd4d9986fd3ed1ac7c17b1c974d14ffbd826c5
James Barry <jbm03003@gmail.com>
2022-01-06 20:42:12 -0500

    added functionality to apis

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,30 @@
 // apiKeyForSpoonacular=62e75e598b60470591e3fd45554b92ca
 // apiKeyForCocktailDb=1 
 
-function getRecipe() {
-    var apiUrl = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=62e75e598b60470591e3fd45554b92ca&ingredients=apples,+flour,+sugar&number=2";
-      fetch(apiUrl)
-        .then(function (response){
-            if (response.ok) {
-                response.json().then(function(data) {
-                    console.log(data);
-                })
-            }
-        })
-}
+
+
+//  function getRecipe() {
+    // var apiUrl = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=62e75e598b60470591e3fd45554b92ca&ingredients=apples,+flour,+sugar&number=2";
+    //   fetch(apiUrl)
+        // .then(function (response){
+            // if (response.ok) {
+                // response.json().then(function(data) {
+                    // console.log(data);
+
+                    // displayRecipe(data);
+                    
+                // })
+            // }
+        // })
+// }
+
+// function displayRecipe(recipe) {
+    // for (var i = 0; i < recipe.length; i++) {
+        // var recipeImage = recipe[i].image;
+        // var recipeName = recipe[i].title;
+        // console.log(recipeImage, recipeName);
+    // }
+// }
 
 function getDrink() {
     var apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php?apiKey=1";
@@ -20,12 +33,20 @@ function getDrink() {
                 if (response.ok) {
                     response.json().then(function(data){
                         console.log(data);
+                        
+                        var cocktail = data.drinks[0].strDrink + "\n"  
+                        + data.drinks[0].strIngredient1; + "\n";
+                        
+                       
+                        
+                        console.log(cocktail);
                     })
                 }
             })
 }
 
-getRecipe();
+
+// getRecipe();
 getDrink();
 
 
